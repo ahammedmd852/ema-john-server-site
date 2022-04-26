6d7e19b359a83154711a164e4c2fede31f2d9917
ahammed <ahammedmd852@gmail.com>
2022-04-27 00:17:04 +0600

    ema john and server has been connected

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8jrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("JOHN IS RUNNING FOR EMA!");
